5f7ad311928d5e832f23329905b7bb6b444007f1
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-06-24 23:09:11 +0530

    feat: added leaderboard page

--- src/pages/leaderboard-page/LeaderBoardPage.jsx
+++ src/pages/leaderboard-page/LeaderBoardPage.jsx
@@ -1,21 +1,69 @@
 import "./leaderboard-page.css";
+import { BsCoin } from "react-icons/bs";
 
 const LeaderBoardPage = () => {
   return (
     <div className="leaderboard-page">
       <div className="leaderboard">
         <div className="leaderboard-head">
-          <h2 className="leaderboard-heading">Leaderboard</h2>
-          <span className="trophy">🏆</span>
-        </div>
-        <div className="leaderboard-card-body">
-          <div className="leaderboard-card-heading">
-            <h2>USERNAME</h2>
-            <h2>RANK</h2>
-            <h2>COINS</h2>
+          <div className="your-score-card">
+            <h3>Your Score</h3>
+            <div className="profile-img-container">
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/002/275/847/original/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg"
+                alt=""
+                className="your-profile-pic"
+              />
+            </div>
+            <h5>opplayer5</h5>
+            <h5>Rank 1</h5>
+            <h5
+              style={{ display: "flex", alignItems: "center", gap: "0.2rem" }}
+            >
+              <BsCoin /> 300 Coins
+            </h5>
           </div>
         </div>
-        <div className="your-score-card"></div>
+        <div className="leaderboard-body">
+          <div className="leaderboard-heading">
+            <h2>Leaderboard 🏆</h2>
+          </div>
+          <div className="leaderboard-col">
+            <h4>NAME</h4>
+            <h4>RANK</h4>
+            <h4>COINS</h4>
+          </div>
+          <div className="leaderboard-col">
+            <h5>gamertank</h5>
+            <h5>1</h5>
+            <h5>300</h5>
+          </div>
+          <div className="leaderboard-col">
+            <h5>gamertank</h5>
+            <h5>1</h5>
+            <h5>300</h5>
+          </div>
+          <div className="leaderboard-col">
+            <h5>gamertank</h5>
+            <h5>1</h5>
+            <h5>300</h5>
+          </div>
+          <div className="leaderboard-col">
+            <h5>gamertank</h5>
+            <h5>1</h5>
+            <h5>300</h5>
+          </div>
+          <div className="leaderboard-col">
+            <h5>gamertank</h5>
+            <h5>1</h5>
+            <h5>300</h5>
+          </div>
+          <div className="leaderboard-col">
+            <h5>gamertank</h5>
+            <h5>1</h5>
+            <h5>300</h5>
+          </div>
+        </div>
       </div>
     </div>
   );
